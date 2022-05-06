b0c48e7ef3111546d8059c22d10c5ed93bba4cca
Rezende00 <pedro.rezende@sou.inteli.edu.br>
2022-05-05 23:55:42 -0300

    Organização das pastas

---
+++ Atividades feitas em Aula/Começo de Javascript/teste.js
@@ -0,0 +1,16 @@
+// comentário aleatório 
+
+function ex1() {
+    // isso aqui embaixo é uma mensagem de aviso
+alert('Deu erro família')
+
+//Pegar o valor do Form 
+var v0 = document.getElementById('v0').value;
+const g = 10; // aceleração gravitacional 10m/s²
+
+var tSubida = v0/g; 
+document.getElementById('bora').innerHTML = "TEMPO DE SUBIDA = " + tSubida;
+console.log("TEMPO DE SUBIDA = " + tSubida); 
+}
+
+alert("TEMPO DE SUBIDA = " + tSubida);
